27eaf39a0e1166196707e677f42ea9105e59fe0b
shaadi1478 <sheikhshaadi137@gmail.com>
2025-10-16 00:10:02 +0600

    BMI calculator

---
+++ script.js
@@ -0,0 +1,38 @@
+
+<script>
+let weight = document.getElementById("weight");
+let height = document.getElementById("height");
+let btn = document.getElementById("btn");
+let result = document.getElementById("result");
+
+btn.addEventListener("click", () => {
+    let w = parseFloat(weight.value);
+    let h = parseFloat(height.value);
+
+    if(!w || !h){
+        result.textContent = "Please enter valid weight and height!";
+        result.className = "";
+        return;
+    }
+
+    let bmi = w / (h * h);
+    bmi = bmi.toFixed(2);
+
+    if(bmi < 18.5){
+        result.textContent = `BMI: ${bmi} — Underweight`;
+        result.className = "underweight";
+    }
+    else if(bmi >= 18.5 && bmi <= 24.9){
+        result.textContent = `BMI: ${bmi} — Normal weight`;
+        result.className = "normal";
+    }
+    else if(bmi >= 25 && bmi <= 29.9){
+        result.textContent = `BMI: ${bmi} — Overweight`;
+        result.className = "overweight";
+    }
+    else {
+        result.textContent = `BMI: ${bmi} — Obese`;
+        result.className = "obese";
+    }
+});
+</script>
